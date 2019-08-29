fc1ce9782ec64d6e3215af5638db6df0f733d9be
0774775 <0774775@KELCE-LABS17-P.pittstate.edu>
2019-08-29 12:09:16 -0500

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,12 @@
  * @author 0774775
  */
 public class Main {
-    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world this is "
+                             + "a long phrase");
+        int funNumber = 5; 
+        String greetingPhrase = ("Hello World");
+        System.out.println(greetingPhrase + "! " + funNumber);
+    }
 }
---
+++ src/main/java/MilesPerGallon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0774775
+ */
+public class MilesPerGallon {
+    public static void main (String[] args)
+    {
+       
+       float milesDriven = 350.05F;
+       float gallonsOfGasUsed = 10;
+       
+       float milesPerGallon = milesDriven/gallonsOfGasUsed;
+       
+       System.out.println("Miles per gallon:\t" + milesPerGallon);
+    }
+    
+}
